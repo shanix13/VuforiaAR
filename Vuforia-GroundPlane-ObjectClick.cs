a3a6371f214e53ff54d2e62873e10270638115fa
X13 <sha_xtest@yahoo.com>
2021-11-17 12:39:31 +0800

    Add files via upload
    Added file

---
+++ Vuforia-GroundPlane-ObjectClick.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    public GameObject panelObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    ShowDetail(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    void ShowDetail(GameObject obj)
+    {
+        switch (obj.name)
+        {
+            case "CubeClick1":
+                if (panelObj.activeSelf)
+                {
+                    panelObj.SetActive(false);
+                }
+                else
+                {
+                    panelObj.SetActive(true);
+                }
+                
+                break;
+
+            default:
+                break;
+        }
+    }
+}
